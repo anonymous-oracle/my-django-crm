5e8c91dd7cef9eed8924916bc2bc81f9e33005b2
anonymous-oracle <suhass149@outlook.com>
2024-10-12 17:11:20 +0530

    saving the leads using the model forms

--- leads/views.py
+++ leads/views.py
@@ -28,14 +28,34 @@ def lead_detail(request: HttpRequest, pk):
     }
     return render(request=request, template_name='leads/lead_detail.html', context=context)
 
+# def lead_create(request: HttpRequest):
+#     if request.method.lower() == 'post':
+#         form = forms.LeadForm(request.POST)
+#         if form.is_valid():
+#             if not form.cleaned_data.get('agent'):
+#                 form.cleaned_data.update(agent = Agent.objects.first())
+#             lead = Lead.objects.create(**form.cleaned_data)
+#             lead.save()
+#             return redirect('/leads')
+#             # return HttpResponse(f"Created a lead for {form.cleaned_data.get('first_name')}")
+#     elif request.method.lower() == 'get':   
+#         context = {
+#             "form": forms.LeadForm()
+#         }
+#         return render(request=request, template_name='leads/lead_create.html', context = context)
+#     else:
+#         response = HttpResponse()
+#         response.status_code = 404
+#         response.content = "<h1>Not found</h1>"
+#         return response
+
 def lead_create(request: HttpRequest):
     if request.method.lower() == 'post':
         form = forms.LeadModelForm(request.POST)
         if form.is_valid():
             if not form.cleaned_data.get('agent'):
                 form.cleaned_data.update(agent = Agent.objects.first())
-            lead = Lead.objects.create(**form.cleaned_data)
-            lead.save()
+            form.save() # for a model form, calling the .save() method after validation is sufficient to save the lead details
             return redirect('/leads')
             # return HttpResponse(f"Created a lead for {form.cleaned_data.get('first_name')}")
     elif request.method.lower() == 'get':   
