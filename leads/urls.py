10d2253110f6afcd2763c5196efc454503515038
anonymous-oracle <suhass149@outlook.com>
2024-10-13 14:39:36 +0530

    added url name spaces in templates instead of hardcoded URLs

--- leads/urls.py
+++ leads/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'leads'
 
 urlpatterns = [
-    path('', views.lead_list, name='leads-home'),
-    path('second/', views.second_home_page, name='leads-second-home'),
-    path('<int:pk>/', views.lead_detail, name='leads-detail'),
-    path('create/', views.lead_create, name='leads-create'),
-    path('<int:pk>/update', views.lead_update, name='leads-update'),
-    path('<int:pk>/delete', views.lead_delete, name='leads-delete')
+    path('', views.lead_list, name='home'),
+    path('second/', views.second_home_page, name='second-home'),
+    path('<int:pk>/', views.lead_detail, name='detail'),
+    path('create/', views.lead_create, name='create'),
+    path('<int:pk>/update', views.lead_update, name='update'),
+    path('<int:pk>/delete', views.lead_delete, name='delete')
 ]
