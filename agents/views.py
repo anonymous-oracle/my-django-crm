a3988e0fe431d7b8d71bc3787e593d6f306d61c9
anonymous-oracle <suhass149@outlook.com>
2024-10-25 21:43:58 +0530

    added update, delete views; to add filtered query set which is user specific

--- agents/views.py
+++ agents/views.py
@@ -25,6 +25,32 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
         agent = form.save(commit=False) # not to commit to the database immediately but to save it in a variable for now
         agent.organisation = self.request.user.userprofile # access to the user object because of the LoginRequiredMixin
         agent.save() # saving it now
+
         
         # since the AgentCreateView form class inherits from the CreateView class, which in turn inherits from the form template mixin class and the BaseCreateView itself, the AgentCreateView class is being passed as the template Mixin class for the super() call and the self class reference of the AgentCreateView class is being passed as the CreateView/BaseCreateview class on which .form_valid function is being called
         return super(AgentCreateView, self).form_valid(form)
+
+class AgentDetailView(LoginRequiredMixin, generic.DetailView):
+    template_name = "agents/agent_detail.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Agent.objects.all()
+    
+class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = "agents/agent_update.html"
+    form_class = AgentModelForm
+
+    def get_success_url(self) -> str:
+        return reverse("agents:agent-list")
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Agent.objects.all()
+
+class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
+    template_name = "agents/agent_delete.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Agent.objects.all()
+    
+    def get_success_url(self) -> str:
+        return reverse("agents:agent-list")
