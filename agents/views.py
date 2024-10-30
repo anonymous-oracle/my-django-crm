1da31221e6db5b8f8b5238e3352a7d82a63c88a9
anonymous-oracle <suhass149@outlook.com>
2024-10-30 21:36:20 +0530

    added logic to register agents as User objects and then create an Agent object with the user

--- agents/views.py
+++ agents/views.py
@@ -1,8 +1,10 @@
 from typing import Any
+import random
 from django.db.models.query import QuerySet
 from django.forms import BaseModelForm
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from django.core.mail import send_mail
 from  django.views import generic
 from leads.models import Agent
 from .forms import AgentModelForm
@@ -23,9 +25,25 @@ class AgentCreateView(mixins.OrganiserAndLoginRequiredMixin, generic.CreateView)
         return reverse('agents:agent-list')
     
     def form_valid(self, form: BaseModelForm) -> HttpResponse:
-        agent = form.save(commit=False) # not to commit to the database immediately but to save it in a variable for now
-        agent.organisation = self.request.user.userprofile # access to the user object because of the LoginRequiredMixin
-        agent.save() # saving it now
+        # agent = form.save(commit=False) # not to commit to the database immediately but to save it in a variable for now
+        # agent.organisation = self.request.user.userprofile # access to the user object because of the LoginRequiredMixin
+        # agent.save() # saving it now
+
+        user = form.save(commit=False) # not to commit to the database immediately but to save it in a variable for now
+        user.is_agent = True
+        user.is_organiser = False
+        user.set_password(f"{random.randint(0, 1000000000000)}") # set_password() hashes the password and handles all the encryption logic
+        user.save()
+        Agent.objects.create(
+            user=user,
+            organisation=self.request.user.userprofile
+        )
+        send_mail(
+            subject="You have been added as an agent",
+            message="You were added as an Agent on Django CRM. Please login in order to start working.",
+            from_email="admin@test.com",
+            recipient_list=[user.email]
+        )
 
         
         # since the AgentCreateView form class inherits from the CreateView class, which in turn inherits from the form template mixin class and the BaseCreateView itself, the AgentCreateView class is being passed as the template Mixin class for the super() call and the self class reference of the AgentCreateView class is being passed as the CreateView/BaseCreateview class on which .form_valid function is being called
