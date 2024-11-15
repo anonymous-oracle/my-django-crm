0e9c9224cee0518b0b2156e4d439e8cb50e97719
anonymous-oracle <suhass149@outlook.com>
2024-11-15 08:53:35 +0530

    added runserver script

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -162,3 +162,20 @@ LOGOUT_REDIRECT_URL = '/'
 
 CRISPY_ALLPWED_TEMPLATE_PACK = 'tailwind'
 CRISPY_TEMPLATE_PACK = 'tailwind'
+
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_SECONDS = 31536000 # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    X_FRAME_OPTIONS = "DENY"
+
+    ALLOWED_HOSTS = ["*"] # once a domain is decided for the app, the domain would be specified and practically it is not advisable to allow all hosts
+
+    # In production run the django app using gunicorn
+    # gunicorn djcrm.wsgi:application # this points to the folder in the project root djcrm -> wsgi.py -> application variable
